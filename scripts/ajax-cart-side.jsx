15adaf89989bd4c56d3137a7b63ccd9a53bb8c8b
marsespinosa <mariconespinosa888@gmail.com>
2023-05-22 23:05:01 +0800

    update react

--- scripts/ajax-cart-side.jsx
+++ scripts/ajax-cart-side.jsx
@@ -1,9 +1,48 @@
-import React from "react"
-import ReactDom from "react-dom"
+import React, { useState, useEffect } from 'react';
+import Client from 'shopify-buy';
 
-const Test = () => {
-	return <div>This is tutorial to integrate react.js into shopify theme with webpack</div>
-}
+const ProductPage = () => {
+  const [product, setProduct] = useState(null);
+  const [selectedVariant, setSelectedVariant] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-const root = document.getElementById('cart__drawer_items');
-ReactDom.render(<Test />, root);
+  useEffect(() => {
+    const client = Client.buildClient({
+      storefrontAccessToken: 'YOUR_STOREFRONT_ACCESS_TOKEN',
+      domain: 'YOUR_SHOP_DOMAIN',
+    });
+
+    client.product.fetch('PRODUCT_ID').then((fetchedProduct) => {
+      setProduct(fetchedProduct);
+      setSelectedVariant(fetchedProduct.variants[0]);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const handleVariantChange = (variant) => {
+    setSelectedVariant(variant);
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <img src={selectedVariant.image.src} alt={selectedVariant.image.altText} />
+          <p>{selectedVariant.title}</p>
+          <p>Price: {selectedVariant.price}</p>
+          {product.variants.map((variant) => (
+            <button key={variant.id} onClick={() => handleVariantChange(variant)}>
+              {variant.title}
+            </button>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ProductPage;
